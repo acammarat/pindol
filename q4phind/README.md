74a71cbe2ee9d05b442484b0ed3aa5ae429656e7
acammarat <124535593+acammarat@users.noreply.github.com>
2023-03-17 16:04:44 +0100

    Add files via upload

---
+++ q4phind/README.md
@@ -0,0 +1,14 @@
+# PINDOL
+Phonon-Inspired Normal Dynamics Of Lattices
+
+Suite of codes to perform dynamics in the reciprocal space.
+
+- [**pindol**](https://github.com/acammarat/pindol/tree/main/pindol) Main code that performs dynamics in the NVE and NVT ensembles. 
+
+- [**phind**](https://github.com/acammarat/pindol/tree/main/phind) ...
+
+- [**...**]
+
+## Contributions, bug reports and feature requests
+
+We are happy to accept contributions. To report bugs or request new features, please use the [Issue Tracker](https://github.com/acammarat/pindol/issues). If you use the programs in this repository in your work, please send an email to cammaant [at] fel.cvut.cz - we will collect them and put up a list of outputs.
---
+++ q4phind/show_logo.f90
@@ -0,0 +1,43 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! 
+! q4phind. Copyright (C) Antonio Cammarata                            
+! https://nano.cvut.cz/researchers/antonio-cammarata
+! https://orcid.org/0000-0002-5691-0682
+! 
+! Program to calculate the atomic character
+! Order a q-point set as GM, H, S and remove possible complex-conjugated couples
+! to generate qmatrix.nd and freq.nd compatible with phind v >= 4.3
+! 
+!    This file is part of q4phind.
+!
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine show_logo
+  use var, only: version
+
+  write(*,'(a)')     "       _  _       _                   " 
+  write(*,'(a)')     "  __ _| || |    _| |_              _  "
+  write(*,'(a)')     " / _` | || |_  /     \   _ __   __| | "
+  write(*,'(a)')     "| (_| |__   _|( (| |) ) | '_ \ / _` | "
+  write(*,'(a)')     " \__, |  |_|   \_   _/  | | | | (_| | "
+  write(*,'(a)')     "    |_|          |_|    |_| |_|\__,_| "
+  write(*,'(*(a))')  "                              ",version
+  write(*,*)
+         
+  
+  return
+end subroutine show_logo
